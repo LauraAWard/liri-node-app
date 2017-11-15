17e87998d6f271066cc111eca7f2e629c7f1eccb
LauraAWard <l-ward@northwestern.edu>
2017-11-14 18:54:34 -0600

    updates from PC

--- keys.js
+++ keys.js
@@ -1,10 +1,13 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'm1FbXYsXu9R1Bgk6miHUNWItA',
+  consumer_secret: 'iC5FEN5WxFGl8vZ7eXMcaQOhbfAU6z6J5vBZnjGZOJ4YdCHKP4',
+  access_token_key: '930294411220660226-nkoD2B2fhQC8sk9h87HsRIBwT0RdVYS',
+  access_token_secret: 'ECpdOothsmGJtBJdSJ6Yzr0QxZ2htEgiXklT1IYu359Z0',
 }
 
-module.exports = twitterKeys;
+module.exports = {
+	twitterKeys: twitterKeys
+}
+
--- liri.js
+++ liri.js
@@ -3,3 +3,13 @@
 var twitter = require("twitter");
 var spotify = require("node-spotify-api");
 var request = require("request");
+
+var keys = require("./keys.js");
+
+
+
+var twitterKeys = keys.twitterKeys;
+console.log(twitterKeys);
+
+var command = process.argv[2];
+var parameter = process.argv[3];
